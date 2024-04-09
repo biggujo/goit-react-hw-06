137132743a414d338f08255a2793a9a0d52aec9c
Dmytro Ivashchenko <gen.style22@gmail.com>
2024-04-09 15:10:31 +0300

    Move to Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App/App';
 import 'modern-normalize';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import App from './components/App/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
   <Provider store={store}>
